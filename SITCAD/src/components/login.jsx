a76b4361d90036f84afff7f3c7d747ef6d0fe1cd
Anxiietyx <ivanloong05@gmail.com>
2026-02-12 14:41:33 +0800

    update

--- SITCAD/src/components/login.jsx
+++ SITCAD/src/components/login.jsx
@@ -1,16 +1,22 @@
 
 export function Login() {
     return (
-        <>
-            <h1>This is the login page</h1>
+    <>
+    <body>
+        <h1>This is the login page</h1>
+        <form>
             <label id="Usernames">Username: </label>
             <input id='Usernames' type='text' placeholder="Text here!" required/>
             <br/>
             <br/>
             <label id='Pass'>Password: </label>
-            <input id="Pass" type='password' minLength='5' placeholder="Text here!" required/>
-            
-        </>
+            <input id="Pass" type='password' minLength='5' placeholder="Password here!" required/>
+            <br/>
+            <br/>
+            <button  type='submit' className="">Sign up</button>
+        </form>
+    </body>
+    </>
     )
 }
 
