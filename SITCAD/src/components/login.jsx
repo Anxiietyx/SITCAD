991cda97909abd41e07b9a10dc8c8756c6bfbd4e
Anxiietyx <ivanloong05@gmail.com>
2026-02-12 11:43:59 +0800

    login

--- SITCAD/src/components/login.jsx
+++ SITCAD/src/components/login.jsx
@@ -3,6 +3,12 @@ export function Login() {
     return (
         <>
             <h1>This is the login page</h1>
+            <label id="Usernames">Username: </label>
+            <input id='Usernames' type='text' placeholder="Text here!" required/>
+            <br/>
+            <br/>
+            <label id='Pass'>Password: </label>
+            <input id="Pass" type='password' minLength='5' placeholder="Text here!" required/>
             
         </>
     )
