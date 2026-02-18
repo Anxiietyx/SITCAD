cc6ab933ca6487e6ddba61d4fa2f37579472225f
Chiew Cheng Yi <150312205+ccy7701@users.noreply.github.com>
2026-02-18 16:40:21 +0800

    Update App.jsx
    Fix minor filename issue preventing loading of Homepage route

--- SITCAD/src/App.jsx
+++ SITCAD/src/App.jsx
@@ -1,6 +1,6 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Layout } from './Layout'
-import Home from './Pages/Home'
+import Homepage from './Pages/Homepage'
 import Page1 from './Pages/Page1'
 import Page2 from './Pages/Page2'
 import './App.css'
@@ -15,7 +15,7 @@ function App() {
     <Router>
       <Routes>
         <Route element={<Layout/>}>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Homepage/>}/>
         <Route path='/page1' element={<Page1/>}/>
         <Route path='/page2' element={<Page2/>}/>    
         <Route path='/login' element={<Login/>}/>
